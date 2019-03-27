497cda3bc9bdc79273ea9c3ccda5ef9df3e0332e
Malte Hübner <maltehuebner@gmx.org>
2019-03-27 12:45:24 +0100

    Init bundle.

---
+++ CalderaArt11Bundle.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Caldera\Art11Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class CalderaArt11Bundle extends Bundle
+{
+}
